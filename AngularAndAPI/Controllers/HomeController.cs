6b1fe8d2950b2032f836a1581e51edc1a6f80611
saad muhammed v <saadvavat@gmail.com>
2024-04-30 14:16:03 +0530

    first commit

---
+++ AngularAndAPI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+namespace AngularAndAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult HelloWorld()
+        {
+            var text = JsonSerializer.Serialize("Hello World");
+            return Ok(text);
+        }
+    }
+}
